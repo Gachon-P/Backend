fdbe6dabf6dbb8d755f20daa16b3786327119f3b
Young Min <dldudals0728@gachon.ac.kr>
2022-11-28 17:30:38 +0900

    GPP-11 #in-progress #comments spring boot project start

---
+++ src/main/java/kr/ac/gachon/pproject/PProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.gachon.pproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/gachon/pproject/PProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.gachon.pproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
